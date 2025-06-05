ed7a87e70f1f81cbcd9a3893281e527b3569e46d
Otter963 <averagetoast963@gmail.com>
2025-06-05 23:19:06 +0200

    Dash mechanic working
    Finally got the dash mechanic working, was being weird at first but yeh

--- GameProd2_ME3/Assets/Scripts/Player/PlayerController.cs
+++ GameProd2_ME3/Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 //the player movement script to walk/attack
@@ -11,8 +12,34 @@ public class PlayerController : MonoBehaviour
     //getting player input as a float
     private float xAxis;
 
+    [Header("Ground Check Settings")]
+    //to check when the player is on ground
+    [SerializeField] private Transform groundCheckPoint;
+
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+
+    [SerializeField] private LayerMask whatIsGround;
+
     //a variable for the camera script to track the player
     public static PlayerController instance;
+
+    //to access the playerstatelist script
+    [SerializeField] private PlayerStateList pState;
+
+    [Header("Dash settings")]
+    //dash variables
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+
+    //the bool for if the player is dashing
+    private bool canDash = true;
+    //bool to make sure the player only dashes once and not continuously
+    private bool dashed;
+
+    //to keep reference to the rigid body's scale
+    private float gravity;
 
     private void Awake()
     {
@@ -30,15 +57,18 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gravity = playerRB.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInputs();
+
+        if (pState.dashing) return;
         Move();
         Flip();
+        StartDash();
     }
 
     //getting the input on the horizontal plane from the player
@@ -53,6 +83,41 @@ public class PlayerController : MonoBehaviour
         playerRB.linearVelocity = new Vector2(walkSpeed * xAxis, playerRB.linearVelocity.y);
     }
 
+    //take note, this might make the player dash in the air when not grounded,
+    //so might need to go back to part 1 to set the grounded variable stuff
+    void StartDash()
+    {
+        if (Input.GetButtonDown("Dash") && canDash && !dashed)
+        {
+            Debug.Log("Dash is pressed");
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+
+        //this will be for when the player is touching the ground
+        if (Grounded())
+        {
+            dashed = false;
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+
+        //The following is for when the animations come into play
+        pState.dashing = true; //this is from the pState script made in part 2 of the tutorial series
+        //anim.SetTrigger("Dashing"); //the reference to the animator controller        
+
+        playerRB.gravityScale = 0;
+        playerRB.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        yield return new WaitForSeconds(dashTime);
+        playerRB.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
     //flipping the sprite corresponding to the direction(will be helpful when actual sprites are added
     void Flip()
     {
@@ -63,6 +128,20 @@ public class PlayerController : MonoBehaviour
         else if (xAxis > 0)
         {
             transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) || 
+            Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) ||
+            Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
